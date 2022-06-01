516db9fd3e67c08d2b51f8de936715aefba6a137
Adel LATIBI <alatibi@campus-eni.fr>
2022-06-01 16:18:42 +0200

    exemple pour le tp calc

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,16 +3,18 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController {
 
     /**
-     * @Route("/",name="app_home")
+     * @Route("/azertyui",name="app_accueil")
      */
     public function index():Response{
-        return $this->render("pages/home.html.twig");
+        $a = "Home";
+        return $this->render("pages/home.html.twig",["title" => $a]);
     }
 
 
@@ -21,6 +23,7 @@ class HomeController extends AbstractController {
      */
     public function blog():Response{
         return $this->render("pages/blog.html.twig");
+
     }    
 
     /**
@@ -35,8 +38,23 @@ class HomeController extends AbstractController {
      * @Route("/a-propos",name="app_apropos")
      */
     public function apropos():Response{
+        // redirct("app_blog")
         return $this->render("pages/apropos.html.twig");
     }  
+
+
+
+    /**
+     * @Route("/calc",name="app_calc")
+     */
+    public function calc(Request $request):Response{
+        $op1 = $request->query->get("op1");
+        
+        $result=$op1;
+        return $this->render("pages/calc.html.twig",["result"=>$result]);
+    } 
+
+
 
 
 }
