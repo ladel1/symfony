d65498385f69e2145bd594781f64164dabed5e05
Adel LATIBI <alatibi@campus-eni.fr>
2022-06-07 11:15:32 +0200

    Démo module 6

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -4,7 +4,10 @@ namespace App\Form;
 
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,9 +16,9 @@ class ArticleType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('price') 
+            ->add('name',TextType::class,["label"=>"Nom"])
+            ->add('description',TextareaType::class)
+            ->add('price',NumberType::class,["label"=>"Prix"]) 
             ->add('Ajouter',SubmitType::class)           
         ;
     }
